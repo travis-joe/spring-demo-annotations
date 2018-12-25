96db2b4c890b0f850e9f1fe3da91ddb882e73933
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-12-25 12:31:01 +0800

    85 configuration by java code

---
+++ src/main/java/com/travis/springDemoAnnotations/JavaConfigDemoApp.java
@@ -0,0 +1,14 @@
+package com.travis.springDemoAnnotations;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JavaConfigDemoApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+        Coach coach = context.getBean("tennisCoach", Coach.class);
+        System.out.println(coach.getDailyWorkout());
+        System.out.println(coach.getDailyFortune());
+        context.close();
+    }
+}
---
+++ src/main/java/com/travis/springDemoAnnotations/SportConfig.java
@@ -0,0 +1,9 @@
+package com.travis.springDemoAnnotations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.travis.springDemoAnnotations")
+public class SportConfig {
+}
