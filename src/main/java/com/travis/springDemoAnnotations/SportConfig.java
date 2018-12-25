fb831564df12b3d62ef3d6ac2a486602614a1cdc
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-12-25 17:57:36 +0800

    85 define spring bean by java code

--- src/main/java/com/travis/springDemoAnnotations/SportConfig.java
+++ src/main/java/com/travis/springDemoAnnotations/SportConfig.java
@@ -1,9 +1,19 @@
 package com.travis.springDemoAnnotations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.travis.springDemoAnnotations")
+//@ComponentScan("com.travis.springDemoAnnotations")
 public class SportConfig {
+    @Bean
+    public FortuneService sadFortuneService() {
+        return new SadFortuneService();
+    }
+
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach(sadFortuneService());
+    }
 }
---
+++ src/main/java/com/travis/springDemoAnnotations/SwimCoach.java
@@ -0,0 +1,19 @@
+package com.travis.springDemoAnnotations;
+
+public class SwimCoach implements Coach {
+    private FortuneService fortuneService;
+
+    public SwimCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Swim 1000 meters as a warm up";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+}
